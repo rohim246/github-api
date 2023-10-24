afa70d6ef65fe11e8e2c8be11431fc35ea72f25c
rohim <rohim.jogja246@gmail.com>
2023-10-24 20:28:57 +0700

    add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mb-3">
+      <h1 className="md:text-2xl text-xl font-bold border-b-4 p-2 rounded text-center">
+        GitHub Repositories Explorer
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
